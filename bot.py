6b8fa44d1a84c4d7af2659b67b4463b32fe51b23
dnapoli97 <dnapoli1997@yahoo.com>
2020-04-20 18:01:44 -0400

    first instance of working bot

---
+++ bot.py
@@ -0,0 +1,51 @@
+import tweepy, time, sys, os, dotenv, requests
+from dotenv import load_dotenv
+from twitch import TwitchHelix
+from itertools import islice
+load_dotenv()
+
+
+def get_twitter_env():    
+    consumer_key = os.getenv('CONSUMER_KEY')
+    consumer_secret = os.getenv('CONSUMER_SECRET')
+    access_key = os.getenv('ACCESS_KEY')
+    access_secret = os.getenv('ACCESS_SECRET')
+    return consumer_key, consumer_secret, access_key, access_secret
+
+
+def get_twitch_env():
+    client_key = os.getenv('CLIENT_ID')
+    client_secret = os.getenv('CLIENT_SECRET')
+    return client_key, client_secret
+
+
+def set_twitter_api(consumer_key, consumer_secret, access_key, access_secret):
+    twitter_auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+    twitter_auth.set_access_token(access_key,access_secret)
+    twitter_api = tweepy.API(twitter_auth)
+    return twitter_api
+
+
+def get_top_streams(client):
+    streams_iterator = client.get_streams(page_size=7)
+    i=1
+    top={}
+    for stream in islice(streams_iterator, 0, 7):
+        userName = stream['user_name']
+        viewers = stream['viewer_count']
+        top[str(i)] = (str(i)+'. ' + userName + ' Viewers: ' + '{:,d}').format(viewers)
+        i+=1
+    return top
+
+
+def send_new_tweet(top, api):
+    tweet = '{}\n{}\n{}\n{}\n{}\n{}\n{}'.format(top['1'],top['2'],top['3'],top['4'],top['5'],top['6'],top['7'])
+    api.update_status(tweet)
+
+
+if __name__ == "__main__":
+    api = set_twitter_api(*get_twitter_env())
+    client_key, client_secret = get_twitch_env()
+    client = TwitchHelix(client_id=client_key)
+    send_new_tweet(get_top_streams(client), api)
+
