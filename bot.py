28ecf5474f0f9aed12d29b6b0fa639a9467fccfc
dnapoli97 <dnapoli1997@yahoo.com>
2020-05-22 18:02:38 -0400

    algro tweak

--- bot.py
+++ bot.py
@@ -61,7 +61,7 @@ if __name__ == "__main__":
         if EXPIRE < 3600:
             client, EXPIRE = get_twitch_env()
         now = datetime.datetime.now()
-        if not now.hour == 3 or not now.hour == 4 or not now.hour == 5 or not now.hour == 6 or not now.hour == 7:
+        if not now.hour == 23 or not now.hour == 24 or not now.hour == 1 or not now.hour == 2 or not now.hour == 3:
             send_new_tweet(get_top_streams(client), api)
             duration = datetime.datetime.now() - now
             time.sleep(INTERVAL - duration.total_seconds())
